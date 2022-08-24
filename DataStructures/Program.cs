9ad9a79baed73184fa37bca587c3b54f20db0876
Aman <amankr97111@gmail.com>
2022-08-24 14:59:42 +0530

    adding numbers in linked list

---
+++ DataStructures/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Data Structure Program\n");
+            Console.WriteLine("Press 1 \n1:Linked List.");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option) {
+
+                case 1:
+                    linkedlist Linkedlist = new linkedlist();
+                    Linkedlist.Add(56);
+                    Linkedlist.Add(30);
+                    Linkedlist.Add(70);
+
+                    break;
+            
+            
+            
+            }
+           
+        }
+    }
+}
---
+++ DataStructures/linkedlist.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+     class linkedlist
+    {
+
+        internal node head;
+
+        public void Add(int data)
+        {
+            node Node = new node(data);
+            
+                if (head == null)
+            {
+                head = Node;
+            }
+                else
+            {
+                node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = Node;
+            }
+            Console.WriteLine("{0} added into linked list", Node.data);
+
+        }
+
+        public void Display ()
+        {
+            Console.WriteLine("\nDisplaying Nodes\n");
+            node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty.");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+            Console.WriteLine();
+        }
+
+            }
+
+        }
+    
